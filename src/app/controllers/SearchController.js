9b0a66ecb21b4274288b3e6eafb3276ff0ca3680
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-05-13 17:21:38 -0300

    Filter by techs and position

--- src/app/controllers/SearchController.js
+++ src/app/controllers/SearchController.js
@@ -1,9 +1,32 @@
-import parseStringAsArray from '../helpers/parseStringAsArray';
 import Developer from '../models/Developer';
+import parseStringAsArray from '../helpers/parseStringAsArray';
 
 class SearchController {
   async index(req, res) {
-    const { latitude, longitude, techs } = req.query;
+    const { host_url } = req;
+    const { techs, longitude, latitude } = req.query;
+
+    const developers = await Developer.find(
+      {
+        techs: {
+          $in: Array.isArray(techs) ? techs : parseStringAsArray(techs),
+        },
+        location: {
+          $near: {
+            $geometry: {
+              type: 'Point',
+              coordinates: [longitude, latitude],
+            },
+            $maxDistance: 10000,
+          },
+        },
+      },
+      {
+        'location._id': false,
+        'location.type': false,
+        __v: false,
+      }
+    );
 
     return res.json(
       developers.map(developer => ({
