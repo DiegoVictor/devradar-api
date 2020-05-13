8ada32fa7d355c8607f6feac735b4043f49c06d2
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-05-13 17:18:27 -0300

    Configure projection

--- src/app/services/ExistsDeveloper.js
+++ src/app/services/ExistsDeveloper.js
@@ -1,10 +1,14 @@
-import { badRequest } from '@hapi/boom';
+import { notFound } from '@hapi/boom';
 
 import Developer from '../models/Developer';
 
 class ExistsDeveloper {
   async run({ id }) {
-    const developer = await Developer.findById(id);
+    const developer = await Developer.findById(id, {
+      'location._id': false,
+      'location.type': false,
+      __v: false,
+    }).lean();
     if (!developer) {
       throw notFound('Developer does not exists', { code: 144 });
     }
