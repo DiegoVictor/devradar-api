f101f1dfc855a60e3b5049b6c76105587263b9d4
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-05-22 13:52:49 -0300

    Create custom resolver

--- tests/custom_resolver.js
+++ tests/custom_resolver.js
@@ -1,6 +1,17 @@
 module.exports = (request, options) => {
-  if (request.search(/axios/gi) > -1) {
-    return `${options.rootDir}/mocks/axios.js`;
+  let mocked;
+
+  ['axios', 'socket.io'].every(module => {
+    if (request.search(new RegExp(module, 'gi')) > -1) {
+      mocked = module;
+      return false;
+    }
+    return true;
+  });
+
+  if (mocked) {
+    return `${options.rootDir}/mocks/${mocked}.js`;
   }
+
   return options.defaultResolver(request, options);
 };
