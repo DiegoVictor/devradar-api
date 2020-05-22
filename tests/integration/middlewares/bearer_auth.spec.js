5f2318bb97c6e13e7eb1c6c389067cb2c8658a3e
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-05-22 13:53:15 -0300

    Test middlewares

---
+++ tests/integration/middlewares/bearer_auth.spec.js
@@ -0,0 +1,69 @@
+import faker from 'faker';
+import jwt from 'jsonwebtoken';
+
+import BearerAuth from '../../../src/app/middlewares/BearerAuth';
+
+describe('BearerAuth middleware', () => {
+  const res = {
+    status: jest.fn(() => res),
+    json: jest.fn(response => response),
+  };
+
+  it('should not be able to request without a token', () => {
+    const req = { headers: {} };
+    BearerAuth(req, res, jest.fn()).catch(err => {
+      expect({ ...err }).toStrictEqual({
+        data: { code: 240 },
+        isBoom: true,
+        isServer: false,
+        output: {
+          statusCode: 400,
+          payload: {
+            statusCode: 400,
+            error: 'Bad Request',
+            message: 'Missing authorization token',
+          },
+          headers: {},
+        },
+      });
+    });
+  });
+
+  it('should not be able to request with a invalid token', async () => {
+    const req = {
+      headers: {
+        authorization: `Bearer ${jwt.sign(
+          { id: faker.random.number() },
+          faker.random.alphaNumeric(29),
+          {
+            expiresIn: '-1d',
+          }
+        )}`,
+      },
+    };
+
+    const message = 'Token expired or invalid';
+    BearerAuth(req, res, jest.fn()).catch(err => {
+      expect({ ...err }).toStrictEqual({
+        data: null,
+        isBoom: true,
+        isServer: false,
+        output: {
+          statusCode: 401,
+          payload: {
+            attributes: {
+              code: 241,
+              error: message,
+            },
+            statusCode: 401,
+            error: 'Unauthorized',
+            message,
+          },
+          headers: {
+            'WWW-Authenticate': `sample code="241", error="${message}"`,
+          },
+        },
+      });
+    });
+  });
+});
