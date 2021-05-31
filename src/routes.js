647059b9b4b555969e9e21bfc3f86e03b5bbfb85
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-05-31 18:27:57 -0300

    Remove rate limit and brute force middleware

--- src/routes.js
+++ src/routes.js
@@ -8,22 +8,11 @@ import SearchParamsValidator from './app/validators/SearchParamsValidator';
 import DeveloperValidator from './app/validators/DeveloperValidator';
 import PageValidator from './app/validators/PageValidator';
 
-import RateLimit from './app/middlewares/RateLimit';
-import BearerAuth from './app/middlewares/BearerAuth';
-
-import { BruteForce } from './database/redis';
-import config from './config/bruteforce';
 import SessionController from './app/controllers/SessionController';
 
 const Route = Router();
 
-Route.post(
-  '/sessions',
-  new BruteForce(config).prevent,
-  SessionController.store
-);
-
-Route.use(RateLimit);
+Route.post('/sessions', SessionController.store);
 
 Route.get('/developers', PageValidator, DeveloperController.index);
 Route.get('/developers/:id', IdValidator, DeveloperController.show);
